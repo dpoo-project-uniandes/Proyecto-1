917a7518da489b59b6530fc8000a981a04eba976
JhostinAleck <ja.sanchezm12@uniandes.edu.co>
2023-04-02 14:46:33 -0500

    c

--- Entrega 3/hotel_system/src/hotel_system/models/PMS.java
+++ Entrega 3/hotel_system/src/hotel_system/models/PMS.java
@@ -1,5 +1,5 @@
 package hotel_system.models;
-import java.io.*;
+import java.io.BufferedReader;
 import java.util.ArrayList;
 
 public class PMS {
@@ -10,6 +10,41 @@ public class PMS {
 	private ArrayList<Producto> inventarioProductos;
 	private ArrayList<Servicio> inventarioServicios;
 	
+	
+	public PMS() {
+		this.usuarios = cargarUsuarios();
+		this.inventarioHabitaciones = cargarHabitaciones();
+		this.reservas = cargarReservas();
+		this.registros = cargarEstadias();
+		this.inventarioProductos = cargarProductos();
+		this.inventarioServicios = cargarServicios();
+	}
+	
+	private ArrayList<Usuario> cargarUsuarios(){
+		return usuarios;
+		
+	}
+	
+	private ArrayList<Habitacion> cargarHabitaciones(){
+		return inventarioHabitaciones;
+		
+	}
+	private ArrayList<Reserva> cargarReservas(){
+		return reservas;
+		
+	}
+	private ArrayList<Estadia> cargarEstadias(){
+		return registros;
+		
+	}
+	private ArrayList<Producto> cargarProductos(){
+		return inventarioProductos;
+		
+	}	
+	private ArrayList<Servicio> cargarServicios(){
+		return inventarioServicios;
+		
+	}
 	
 	public void registrar_usuario() {
 		
