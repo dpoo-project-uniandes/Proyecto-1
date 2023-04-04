8b3aaa1d32b7a4341ae845bd4f85ebebb5438875
EstebanCastel <111070716+EstebanCastel@users.noreply.github.com>
2023-04-04 17:18:21 -0500

    Cargar productos (finalizado) y servicios (falta solucionar errores)

--- Entrega 3/hotel_system/src/hotel_system/controllers/HotelManagementSystem.java
+++ Entrega 3/hotel_system/src/hotel_system/controllers/HotelManagementSystem.java
@@ -11,9 +11,11 @@ import hotel_system.models.Disponibilidad;
 import hotel_system.models.Estadia;
 import hotel_system.models.Habitacion;
 import hotel_system.models.Producto;
+import hotel_system.models.ProductoRestaurante;
 import hotel_system.models.Reserva;
 import hotel_system.models.Rol;
 import hotel_system.models.Servicio;
+import hotel_system.models.Spa;
 import hotel_system.models.TipoHabitacion;
 import hotel_system.models.Usuario;
 import hotel_system.utils.Utils;
@@ -36,8 +38,8 @@ public class HotelManagementSystem {
 			this.usuarios = cargarUsuarios();
 			this.reservas = cargarReservas();
 			this.registros = cargarEstadias();
-			this.inventarioProductos = cargarProductos();
-			this.inventarioServicios = cargarServicios();
+			this.setInventarioProductos(cargarProductos());
+			this.setInventarioServicios(cargarServicios());
 		} catch (Exception e) {
 			System.out.println("El sistema no puedo iniciarlizarse, intente nuevamente");
 			e.printStackTrace();
@@ -80,7 +82,7 @@ public class HotelManagementSystem {
 			Habitacion habitacion = new Habitacion(numeroHabitacion, tipo, disponibilidad);
 			habitaciones.add(habitacion);
 		}
-		this.inventarioHabitaciones = habitaciones;
+		this.setInventarioHabitaciones(habitaciones);
 	}
 	
 	private List<Disponibilidad> cargarDisponibilidades(Integer numeroHabitacion, Double precio) throws Exception {
@@ -107,14 +109,54 @@ public class HotelManagementSystem {
 		return registros;
 		
 	}
-	private List<Producto> cargarProductos(){
-		return inventarioProductos;
-		
-	}	
-	private List<Servicio> cargarServicios(){
-		return inventarioServicios;
-		
-	}
+	
+
+	private List<Producto> cargarProductos() throws Exception {
+		List<Producto> productosCargados = new ArrayList<>();
+	    List<Map<String, String>> datos = FileManager.cargarArchivoCSV("productos.csv");
+	    for (Map<String, String> dato : datos) {
+	    	Long id = Long.parseLong(dato.get("id"));
+	        String nombre = dato.get("nombre");
+	        Double precio = Double.parseDouble(dato.get("precio"));
+	        Producto producto = new Producto(id, nombre, precio);
+	        productosCargados.add(producto);
+	        }
+	    this.setInventarioProductos(productosCargados);
+	    return productosCargados;
+	    }
+
+	    
+	    private void cargarServicios() throws Exception {
+	        HashMap<String, List<Object>> serviciosCargados = new HashMap<>();
+
+	        List<Object> productosSpa = new ArrayList<>();
+	        List<Map<String, String>> datosSpa = FileManager.cargarArchivoCSV("productos_spa.csv");
+	        for (Map<String, String> dato : datosSpa) {
+	            Long id = Long.parseLong(dato.get("id"));
+	            String nombre = dato.get("nombre");
+	            Double precio = Double.parseDouble(dato.get("precio"));
+	            Spa producto = new Spa(id, nombre, precio);
+	            productosSpa.add(producto);
+	        }
+	        serviciosCargados.put("Spa", productosSpa);
+
+	        List<Object> productosRestaurante = new ArrayList<>();
+	        List<Map<String, String>> datosRestaurante = FileManager.cargarArchivoCSV("productos_restaurante.csv");
+	        for (Map<String, String> dato : datosRestaurante) {
+	            Long id = Long.parseLong(dato.get("id"));
+	            String nombre = dato.get("nombre");
+	            Double precio = Double.parseDouble(dato.get("precio"));
+	            String rangoHorario = dato.get("rangoHorario");
+	            Boolean alCuarto = Boolean.parseBoolean(dato.get("alCuarto"));
+	            String tipo = dato.get("tipo");
+	            ProductoRestaurante producto = new ProductoRestaurante(id, nombre, precio, rangoHorario, alCuarto, tipo);
+	            productosRestaurante.add(producto);
+	        }
+	        serviciosCargados.put("Restaurante", productosRestaurante);
+
+	        this.setInventarioServicios(serviciosCargados);
+	    }
+
 	
 	public boolean validad_usuario(String user) {
 		return usuarios.containsKey(user);
@@ -126,5 +168,29 @@ public class HotelManagementSystem {
 	
 	public void registrar_usuario(String user, String password, Rol rol) {
 		usuarios.put(user, new Usuario(user, password, rol));
+	}
+
+	public List<Habitacion> getInventarioHabitaciones() {
+		return inventarioHabitaciones;
+	}
+
+	public void setInventarioHabitaciones(List<Habitacion> inventarioHabitaciones) {
+		this.inventarioHabitaciones = inventarioHabitaciones;
+	}
+
+	public List<Producto> getInventarioProductos() {
+		return inventarioProductos;
+	}
+
+	public void setInventarioProductos(List<Producto> inventarioProductos) {
+		this.inventarioProductos = inventarioProductos;
+	}
+
+	public List<Servicio> getInventarioServicios() {
+		return inventarioServicios;
+	}
+
+	public void setInventarioServicios(List<Servicio> inventarioServicios) {
+		this.inventarioServicios = inventarioServicios;
 	}
 }
